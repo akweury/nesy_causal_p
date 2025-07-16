74d8fc85d84bbaf1d1dc8cb27fabf8478e724141
jing <shajingyuan@me.com>
2025-07-16 16:24:25 +0200

    balance labels

--- mbg/scorer/context_proximity_dataset.py
+++ mbg/scorer/context_proximity_dataset.py
@@ -72,34 +72,23 @@ class ContextContourDataset(Dataset):
             objects, obj_imgs, permutes = patch_preprocess.align_data_and_imgs(objects, obj_imgs)
             self._process_object_pairs(objects, obj_imgs, device)
 
-    def _process_object_pairs(self, objects, obj_imgs, device="cpu"):
+    def _process_object_pairs(self, objects, obj_imgs, device="cpu", sample_size=10):
         patches = [patch_preprocess.rgb2patch(img, self.input_type)[0].to(device) for img in obj_imgs]
-        for i in range(len(objects)):
-            for j in range(len(objects)):
-                if i == j:
-                    continue
-                obj_i = objects[i]
-                obj_j = objects[j]
-                c_i = patches[i]
-                c_j = patches[j]
-                # c_i, _, _ = patch_preprocess.rgb2patch(obj_imgs[i], self.input_type)
-                # c_j, _, _ = patch_preprocess.rgb2patch(obj_imgs[j], self.input_type)
-                others = [patches[k] for k in range(len(objects)) if k!=i and k!=j]
-                if others:
-                    others_tensor = torch.stack(others, dim=0).to(device)
-                else:
-                    others_tensor = torch.zeros((1, 6, 16, c_i.shape[-1]), device=device)
-
-                # for k in range(len(objects)):
-                #     if k != i and k != j:
-                #         c_k, _, _ = patch_preprocess.rgb2patch(obj_imgs[k], self.input_type)
-                #         others.append(c_k)
-                # if others:
-                #     others_tensor = torch.stack(others, dim=0)
-                # else:
-                #     others_tensor = torch.zeros((1, 6, 16, c_i.shape[-1]))
-                label = 1 if obj_i["group_id"] == obj_j["group_id"] and obj_i["group_id"] != -1 else 0
-                self.data.append((c_i, c_j, others_tensor, label))
+        pair_indices = [(i, j) for i in range(len(objects)) for j in range(len(objects)) if i != j]
+        if len(pair_indices) > sample_size:
+            pair_indices = random.sample(pair_indices, sample_size)
+        for i, j in pair_indices:
+            obj_i = objects[i]
+            obj_j = objects[j]
+            c_i = patches[i]
+            c_j = patches[j]
+            others = [patches[k] for k in range(len(objects)) if k != i and k != j]
+            if others:
+                others_tensor = torch.stack(others, dim=0).to(device)
+            else:
+                others_tensor = torch.zeros((1, 6, 16, c_i.shape[-1]), device=device)
+            label = 1 if obj_i["group_id"] == obj_j["group_id"] and obj_i["group_id"] != -1 else 0
+            self.data.append((c_i, c_j, others_tensor, label))
 
 
     def _load(self, device="cpu"):
